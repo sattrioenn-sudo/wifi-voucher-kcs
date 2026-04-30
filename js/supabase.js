78d77aa88472053fd3f5aae29b23d039b4fa8418
sattrioenn-sudo <sattrioenn@gmail.com>
2026-05-01 05:31:40 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,5 @@
+// Ganti dengan detail dari Project Settings > API di dashboard Supabase kamu
+const SUPABASE_URL = 'https://ihqyxkbnfapxumvmrkwx.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlocXl4a2JuZmFweHVtdm1ya3d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNzYzMTAsImV4cCI6MjA4NzY1MjMxMH0.fNAimf5ebCRxcBuN6LZIctEUIohwdrfummhy1tCKANk';
 
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
